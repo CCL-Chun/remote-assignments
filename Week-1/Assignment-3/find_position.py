e73b37036f43330900824f4fc81012e335722a54
CCL-Chun <kevin10218427@gmail.com>
2024-01-17 15:29:03 +0800

    Refactor code to follow PEP 8 guidelines and add input array recognition

--- Week-1/Assignment-3/find_position.py
+++ Week-1/Assignment-3/find_position.py
@@ -1,8 +1,10 @@
-def find_position(numbers,target):
-  index=0
-  for i in numbers:
-    if i == target:
-      return index
-    else:
-      index += 1
-  return -1
+def find_position(numbers, target):
+    if not numbers:
+        return "please enter at least 1 number"
+    index = 0
+    for i in numbers:
+        if i == target:
+            return index
+        else:
+            index += 1
+    return -1
